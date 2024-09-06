c76afbc350f2a92eea3d0571a562ffbf309e1dd5
星辰德鲁伊 <xingchendelu1@outlook.com>
2024-09-06 22:56:46 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xingchendelu1_20240906_SMARTenough
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
